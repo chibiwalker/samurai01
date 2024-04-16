7f8ba6dd4f79fdcc6021072fa462fbe92bbd4ab3
chibiwalker <chibiwalker@bk.ru>
2024-04-16 16:38:07 +0300

    ama donkey

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -2,52 +2,52 @@ import React from "react";
 import userPhoto from "../../assets/images/defaultUserPhoto.png"
 import s from "./Users.module.css"
 import axios from 'axios'
-// let users=[
-//     {
-//         "name": "Saved Messages",
-//         "id": 0,
-//         "uniqueUrlName": null,
-//         "photos": {
-//             "small": '../../avatars/0.jpg',
-//             "large": null
-//         },
-//         "status": null,
-//         "followed": false
-//     },
-//     {
-//         "name": "Lonely Wolf",
-//         "id": 1,
-//         "uniqueUrlName": null,
-//         "photos": {
-//             "small": '../../avatars/1.jpg',
-//             "large": null
-//         },
-//         "status": null,
-//         "followed": false
-//     },
-//     {
-//         "name": "sadgirl",
-//         "id": 2,
-//         "uniqueUrlName": null,
-//         "photos": {
-//             "small": '../../avatars/2.jpg',
-//             "large": null
-//         },
-//         "status": null,
-//         "followed": false
-//     },
-//     {
-//         "name": "Ded Inside",
-//         "id": 3,
-//         "uniqueUrlName": null,
-//         "photos": {
-//             "small":'../../avatars/3.jpg',
-//             "large": null
-//         },
-//         "status": null,
-//         "followed": false
-//     }
-//     ]
+let users=[
+    {
+        "name": "Saved Messages",
+        "id": 0,
+        "uniqueUrlName": null,
+        "photos": {
+            "small": '../../avatars/0.jpg',
+            "large": null
+        },
+        "status": null,
+        "followed": false
+    },
+    {
+        "name": "Lonely Wolf",
+        "id": 1,
+        "uniqueUrlName": null,
+        "photos": {
+            "small": '../../avatars/1.jpg',
+            "large": null
+        },
+        "status": null,
+        "followed": false
+    },
+    {
+        "name": "sadgirl",
+        "id": 2,
+        "uniqueUrlName": null,
+        "photos": {
+            "small": '../../avatars/2.jpg',
+            "large": null
+        },
+        "status": null,
+        "followed": false
+    },
+    {
+        "name": "Ded Inside",
+        "id": 3,
+        "uniqueUrlName": null,
+        "photos": {
+            "small":'../../avatars/3.jpg',
+            "large": null
+        },
+        "status": null,
+        "followed": false
+    }
+    ]
 class Users extends React.Component{
     constructor(props){
         super(props);
@@ -65,17 +65,17 @@ class Users extends React.Component{
     
     render(){ 
          return (  
-            this.props.users.map(u=> <div className={s.userTable} key={u.id}>
+            users.map(u=> <div className={s.userTable} key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small!=null? u.photos.small:userPhoto} className={s.userimg}/>
                     </div>
                     <div>
                     {u.followed?
-                    <button onClick={this.props.unfollow}>
+                    <button onClick={this.props.unfollow(u.id)}>
                         Unfollow
                     </button>:
-                    <button onClick={this.props.follow}>
+                    <button onClick={this.props.follow(u.id)}>
                     Follow
                     </button>
                         }
--- src/redux/UsersReducer.js
+++ src/redux/UsersReducer.js
@@ -6,61 +6,62 @@ const SET_USERS="SET_USERS";
 // let setUsersAC=(users)=>{return {type:SET_USERS, users}};
 let initialState={
     users:[
-        {
-            "name": "Saved Messages",
-            "id": 0,
-            "uniqueUrlName": null,
-            "photos": {
-                "small": '../../avatars/0.jpg',
-                "large": null
-            },
-            "status": null,
-            "followed": false
-        },
-        {
-            "name": "Lonely Wolf",
-            "id": 1,
-            "uniqueUrlName": null,
-            "photos": {
-                "small": '../../avatars/1.jpg',
-                "large": null
-            },
-            "status": null,
-            "followed": false
-        },
-        {
-            "name": "sadgirl",
-            "id": 2,
-            "uniqueUrlName": null,
-            "photos": {
-                "small": '../../avatars/2.jpg',
-                "large": null
-            },
-            "status": null,
-            "followed": false
-        },
-        {
-            "name": "Ded Inside",
-            "id": 3,
-            "uniqueUrlName": null,
-            "photos": {
-                "small":'../../avatars/3.jpg',
-                "large": null
-            },
-            "status": null,
-            "followed": false
-        }
+        // {
+        //     "name": "Saved Messages",
+        //     "id": 0,
+        //     "uniqueUrlName": null,
+        //     "photos": {
+        //         "small": '../../avatars/0.jpg',
+        //         "large": null
+        //     },
+        //     "status": null,
+        //     "followed": false
+        // },
+        // {
+        //     "name": "Lonely Wolf",
+        //     "id": 1,
+        //     "uniqueUrlName": null,
+        //     "photos": {
+        //         "small": '../../avatars/1.jpg',
+        //         "large": null
+        //     },
+        //     "status": null,
+        //     "followed": false
+        // },
+        // {
+        //     "name": "sadgirl",
+        //     "id": 2,
+        //     "uniqueUrlName": null,
+        //     "photos": {
+        //         "small": '../../avatars/2.jpg',
+        //         "large": null
+        //     },
+        //     "status": null,
+        //     "followed": false
+        // },
+        // {
+        //     "name": "Ded Inside",
+        //     "id": 3,
+        //     "uniqueUrlName": null,
+        //     "photos": {
+        //         "small":'../../avatars/3.jpg',
+        //         "large": null
+        //     },
+        //     "status": null,
+        //     "followed": false
+        // }
         ]}
     
 const UsersReducer = (state=initialState, action) => {
     switch (action.type) {
         case FOLLOW:
-            alert("follow")
             return {
             ...state,
             users: state.users.map(u=>{
-                if (u.id === action.userId)
-                    return {...u, followed: true}
+                if (u.id === action.userId){
+                    alert("gotcha")
+                    return {...u, followed: true}}
+                alert("damn")
                 return u;
         })
     };

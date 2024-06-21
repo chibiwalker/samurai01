2989e3030340410073036e70e0cdc7936b6dfadf
chibiwalker <chibiwalker@bk.ru>
2024-06-21 12:18:28 +0300

    some minor changes

--- src/components/users/UsersContainer.jsx
+++ src/components/users/UsersContainer.jsx
@@ -2,27 +2,40 @@ import React from "react";
 import Users from "./Users";
 import { connect } from "react-redux";
 import axios from "axios";
-
+import preloaderImg from "../../assets/images/preloader.gif";
+import s from "./Users.module.css";
+import {
+  follow,
+  unfollow,
+  SetUsers,
+  setTotalCount,
+  SelectPage,
+  tooglePreloader,
+} from "../../redux/UsersReducer";
 class UsersComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = { inputValue: undefined };
   }
   componentDidMount() {
+    this.props.tooglePreloader(true);
     axios
       .get(
         `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.page}`
       )
       .then((response) => {
+        this.props.tooglePreloader(false);
         this.props.SetUsers(response.data.items);
         this.props.setTotalCount(response.data.totalCount);
       });
   }
   onPageChange = (page) => {
     this.props.SelectPage(page);
+    this.props.tooglePreloader(true);
     axios
       .get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}`)
       .then((response) => {
+        this.props.tooglePreloader(false);
         this.props.SetUsers(response.data.items);
       });
   };
@@ -32,14 +45,21 @@ class UsersComponent extends React.Component {
   };
   render() {
     return (
-      <Users
-        handleInputChange={this.handleInputChange}
-        state={this.state}
-        users={this.props.users}
-        totalCount={this.props.totalCount}
-        page={this.props.page}
-        onPageChange={this.onPageChange}
-      />
+      <>
+        {this.props.isFetching && (
+          <img className={s.preloaderImg} src={preloaderImg} />
+        )}
+        <Users
+          handleInputChange={this.handleInputChange}
+          state={this.state}
+          users={this.props.users}
+          totalCount={this.props.totalCount}
+          page={this.props.page}
+          onPageChange={this.onPageChange}
+          follow={this.props.follow}
+          unfollow={this.props.unfollow}
+        />
+      </>
     );
   }
 }
@@ -48,25 +68,32 @@ let mapStateToProps = (state) => {
     users: state.Users.users,
     page: state.Users.page,
     totalCount: state.Users.totalCount,
+    isFetching: state.Users.isFetching,
   };
 };
-let mapDispatchToProps = (dispatch) => {
-  return {
-    follow: (userId) => dispatch({ type: "FOLLOW", userId: userId }),
-    unfollow: (userId) => dispatch({ type: "UNFOLLOW", userId: userId }),
-    SetUsers: (action) =>
-      dispatch({
-        type: "SET_USERS",
-        users: action,
-      }),
-    setTotalCount: (action) =>
-      dispatch({ type: "SET_TOTAL_COUNT", totalCount: action }),
-    SelectPage: (page) => dispatch({ type: "SELECT_PAGE", page: page }),
-  };
-};
-const UsersContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(UsersComponent);
+// let mapDispatchToProps = (dispatch) => {
+//   return {
+//     follow: (userId) => dispatch({ type: "FOLLOW", userId: userId }),
+//     unfollow: (userId) => dispatch({ type: "UNFOLLOW", userId: userId }),
+//     SetUsers: (action) =>
+//       dispatch({
+//         type: "SET_USERS",
+//         users: action,
+//       }),
+//     setTotalCount: (action) =>
+//       dispatch({ type: "SET_TOTAL_COUNT", totalCount: action }),
+//     SelectPage: (page) => dispatch({ type: "SELECT_PAGE", page: page }),
+//     tooglePreloader: (action) =>
+//       dispatch({ type: "TOOGLE_PRELOADER", isFetching: action }),
+//   };
+// };
+const UsersContainer = connect(mapStateToProps, {
+  follow,
+  unfollow,
+  SetUsers,
+  setTotalCount,
+  SelectPage,
+  tooglePreloader,
+})(UsersComponent);
 
 export default UsersContainer;
--- src/redux/UsersReducer.js
+++ src/redux/UsersReducer.js
@@ -1,109 +1,129 @@
-const FOLLOW="FOLLOW";
-const UNFOLLOW="UNFOLLOW";
-const SET_USERS="SET_USERS";
-const SET_FRIENDS="SET_FRIENDS";
-const SELECT_PAGE="SELECT_PAGE";
-const SET_TOTAL_COUNT="SET_TOTAL_COUNT";
-let initialState={
-    totalCount:0,
-    page:1,
-    friends:[],
-    users:[
-        {
-            "name": "Saved Messages",
-            "id": 0,
-            "uniqueUrlName": null,
-            "photos": {
-                "small": '../../avatars/0.jpg',
-                "large": null
-            },
-            "status": "Bounty Hunter from Lone Wolves",
-            "followed": true
-        },
-        {
-            "name": "Lonely Wolf",
-            "id": 1,
-            "uniqueUrlName": null,
-            "photos": {
-                "small": '../../avatars/1.jpg',
-                "large": null
-            },
-            "status": "Just a lonely wolf on a lonely road alone",
-            "followed": true
-        },
-        {
-            "name": "sadgirl",
-            "id": 2,
-            "uniqueUrlName": null,
-            "photos": {
-                "small": '../../avatars/2.jpg',
-                "large": null
-            },
-            "status": "Pop-musician from Lawse",
-            "followed": true
-        },
-        {
-            "name": "Ded Inside",
-            "id": 3,
-            "uniqueUrlName": null,
-            "photos": {
-                "small":'../../avatars/3.jpg',
-                "large": null
-            },
-            "status": "The last living unded inside and outside too",
-            "followed": true
-        },
-        {
-            "name": "Bro",
-            "id": 4,
-            "uniqueUrlName": null,
-            "photos": {
-                "small":'../../avatars/4.jpg',
-                "large": null
-            },
-            "status": "Noble citizen from famous Gorin's house" ,
-            "followed": true
-        }
-        ]}
-    
-const UsersReducer = (state=initialState, action) => {
-    switch (action.type) {
-        case FOLLOW:{
-            const newState={...state,
-                users: state.users.map(u=>{
-                    if (u.id === action.userId){
-                        return {...u, followed: true}}
-                    return u;
-            })
-        };
-        console.log(newState);
-            return newState;}
-            
-        case UNFOLLOW:
-            return{
-                ...state,
-                users: state.users.map(u=>{
-                    if (u.id === action.userId)
-                        return {...u, followed:false}
-                return u;
-        })
-    };
-        case SET_USERS:{
-                return {...state, users:[...initialState.users, ...action.users]
-                }
-            };
-        case SET_FRIENDS:{
-            return {...state, friends:[...initialState.users, ...action.users]}
-        };
-        case SELECT_PAGE:{
-        return {...state, page: action.page}
-        };
-        case SET_TOTAL_COUNT:{
-            return {...state, totalCount:action.totalCount}
-        }
-        default:
-            return state;
-    
+const FOLLOW = "FOLLOW";
+const UNFOLLOW = "UNFOLLOW";
+const SET_USERS = "SET_USERS";
+const SET_FRIENDS = "SET_FRIENDS";
+const SELECT_PAGE = "SELECT_PAGE";
+const SET_TOTAL_COUNT = "SET_TOTAL_COUNT";
+const TOOGLE_PRELOADER = "TOOGLE_PRELOADER";
+let initialState = {
+  totalCount: 0,
+  page: 1,
+  isFetching: true,
+  friends: [],
+  users: [
+    {
+      name: "Saved Messages",
+      id: 0,
+      uniqueUrlName: null,
+      photos: {
+        small: "../../avatars/0.jpg",
+        large: null,
+      },
+      status: "Bounty Hunter from Lone Wolves",
+      followed: true,
+    },
+    {
+      name: "Lonely Wolf",
+      id: 1,
+      uniqueUrlName: null,
+      photos: {
+        small: "../../avatars/1.jpg",
+        large: null,
+      },
+      status: "Just a lonely wolf on a lonely road alone",
+      followed: true,
+    },
+    {
+      name: "sadgirl",
+      id: 2,
+      uniqueUrlName: null,
+      photos: {
+        small: "../../avatars/2.jpg",
+        large: null,
+      },
+      status: "Pop-musician from Lawse",
+      followed: true,
+    },
+    {
+      name: "Ded Inside",
+      id: 3,
+      uniqueUrlName: null,
+      photos: {
+        small: "../../avatars/3.jpg",
+        large: null,
+      },
+      status: "The last living unded inside and outside too",
+      followed: true,
+    },
+    {
+      name: "Bro",
+      id: 4,
+      uniqueUrlName: null,
+      photos: {
+        small: "../../avatars/4.jpg",
+        large: null,
+      },
+      status: "Noble citizen from famous Gorin's house",
+      followed: true,
+    },
+  ],
+};
+
+const UsersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FOLLOW: {
+      const newState = {
+        ...state,
+        users: state.users.map((u) => {
+          if (u.id === action.userId) {
+            return { ...u, followed: true };
+          }
+          return u;
+        }),
+      };
+      console.log(newState);
+      return newState;
     }
-}
+
+    case UNFOLLOW:
+      return {
+        ...state,
+        users: state.users.map((u) => {
+          if (u.id === action.userId) return { ...u, followed: false };
+          return u;
+        }),
+      };
+    case SET_USERS: {
+      return { ...state, users: [...initialState.users, ...action.users] };
+    }
+    case SET_FRIENDS: {
+      return { ...state, friends: [...initialState.users, ...action.users] };
+    }
+    case SELECT_PAGE: {
+      return { ...state, page: action.page };
+    }
+    case SET_TOTAL_COUNT: {
+      return { ...state, totalCount: action.totalCount };
+    }
+    case TOOGLE_PRELOADER: {
+      return { ...state, isFetching: action.isFetching };
+    }
+    default:
+      return state;
+  }
+};
+
+export const follow = (userId) => ({ type: FOLLOW, userId });
+export const unfollow = (userId) => ({ type: UNFOLLOW, userId });
+export const SetUsers = (users) => ({ type: SET_USERS, users });
+export const setTotalCount = (totalCount) => ({
+  type: SET_TOTAL_COUNT,
+  totalCount,
+});
+export const SelectPage = (page) => ({ type: SELECT_PAGE, page });
+export const tooglePreloader = (isFetching) => ({
+  type: TOOGLE_PRELOADER,
+  isFetching,
+});
+
 export default UsersReducer;

51b93f944dba5fc41a84d25c06fdc77f0d7203bd
chibiwalker <chibiwalker@bk.ru>
2024-06-21 10:17:57 +0300

    Class component moved from Users to UsersContainer

--- src/components/users/UsersContainer.jsx
+++ src/components/users/UsersContainer.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 import Users from "./Users";
 import { connect } from "react-redux";
+import axios from "axios";
 
+class UsersComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { inputValue: undefined };
+  }
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.page}`
+      )
+      .then((response) => {
+        this.props.SetUsers(response.data.items);
+        this.props.setTotalCount(response.data.totalCount);
+      });
+  }
+  onPageChange = (page) => {
+    this.props.SelectPage(page);
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}`)
+      .then((response) => {
+        this.props.SetUsers(response.data.items);
+      });
+  };
+  handleInputChange = (event) => {
+    const inputValue = Number(event.target.value);
+    this.setState({ inputValue });
+  };
+  render() {
+    return (
+      <Users
+        handleInputChange={this.handleInputChange}
+        state={this.state}
+        users={this.props.users}
+        totalCount={this.props.totalCount}
+        page={this.props.page}
+        onPageChange={this.onPageChange}
+      />
+    );
+  }
+}
 let mapStateToProps = (state) => {
   return {
     users: state.Users.users,
@@ -23,6 +64,9 @@ let mapDispatchToProps = (dispatch) => {
     SelectPage: (page) => dispatch({ type: "SELECT_PAGE", page: page }),
   };
 };
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+const UsersContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UsersComponent);
 
 export default UsersContainer;

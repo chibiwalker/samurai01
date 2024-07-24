f4862f25c4fd556f85a249f92f622eff58a55d43
chibiwalker <chibiwalker@bk.ru>
2024-07-24 15:59:13 +0300

    useEffect hook moved to profile component

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import s from "./profile.module.css";
 import AddPost from "./posts/AddPost/AddPost";
 import MyPost from "./posts/MyPost/MyPost";
 import UserInfo from "./UserInfo/UserInfo";
+import axios from "axios";
 let Profile = (props) => {
+  useEffect(() => {
+    props.tooglePreloader(true);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/profile/${
+          props.profile.id + 2
+        }`
+      )
+      .then((response) => {
+        props.tooglePreloader(false);
+        props.setProfile(response.data);
+      });
+  }, []);
   let printPosts = props.posts.map((m) => (
     <MyPost message={m.message} id={m.id} author={m.author} />
   ));
--- src/components/profile/ProfileContainer.jsx
+++ src/components/profile/ProfileContainer.jsx
@@ -1,30 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Profile from "./Profile";
 import { connect } from "react-redux";
 import { setProfile, addPost, updatePost } from "../../redux/ProfileReducer";
-import axios from "axios";
 import { tooglePreloader } from "../../redux/UsersReducer";
-
-const ProfileComponent = (props) => {
-  useEffect(() => {
-    props.tooglePreloader(true);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/profile/${
-          this.props.profile.id + 2
-        }`
-      )
-      .then((response) => {
-        props.tooglePreloader(false);
-        props.setProfile(response.data);
-      });
-  }, []);
-  return (
-    <div>
-      <Profile {...props} profile={props.profile} />
-    </div>
-  );
-};
 
 const mapStateToProps = (state) => {
   return {
@@ -40,6 +18,6 @@ const ProfileContainer = connect(mapStateToProps, {
   updatePost,
   setProfile,
   tooglePreloader,
-})(ProfileComponent);
+})(Profile);
 
 export default ProfileContainer;
